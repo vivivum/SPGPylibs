83b075b7ad2d59746c520668a7fc428da11deaa5
vivivum <d.orozco.suarez@gmail.com>
2022-10-06 13:43:46 +0200

    Added documentation in tools.py

--- SPGPylibs/PHItools/tools.py
+++ SPGPylibs/PHItools/tools.py
@@ -1,6 +1,10 @@
 import os, time, functools
 
+
 class bcolors:
+    """
+    This is a simple class for colors.
+    """
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -14,26 +18,41 @@ class bcolors:
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
 
-def printc(*args, color = bcolors.RESET, **kwargs):
-    """My custom print() function."""
-    print(u"\u001b"+f"{color}", end='\r')
+
+def printc(*args, color=bcolors.RESET, **kwargs):
+    """
+    This function wraps the python ``print()`` functions adding color capabilities.
+
+    :param args: params to pass through to ``print()`` function.
+    :param color: provide the text color
+    :param kwargs: **kwargs enables printc to retain all ``print()`` functionality
+    :return: nothing
+    """
+    print(u"\u001b" + f"{color}", end='\r')
     print(*args, **kwargs)
-    print(u"\u001b"+f"{bcolors.RESET}", end='\r')
-    return 
+    print(u"\u001b" + f"{bcolors.RESET}", end='\r')
+    return
+
 
 def countcalls(fn):
-    "decorator function count function calls. Use @countcalls above def "
+    """
+    Decorator function count function calls. Use @countcalls above def.
+    """
 
     @functools.wraps(fn)
     def wrapped(*args):
-        wrapped.ncalls +=1
+        wrapped.ncalls += 1
         return fn(*args)
 
     wrapped.ncalls = 0
     return wrapped
 
+
 def timeit(method):
-    '''helper function to calculate executing time. Use @timeit above def'''
+    """
+    Helper function to calculate executing time. Use @timeit above def.
+    """
+
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
@@ -46,18 +65,27 @@ def timeit(method):
             t = (te - ts) * 1000
             if t > 1e3:
                 print('%r  %2.2f seconds' %
-                      (method.__name__, t/1000.))
+                      (method.__name__, t / 1000.))
             elif t > 1e6:
                 print('%r  %2.2f mim' %
-                      (method.__name__, t/1000./60.))
+                      (method.__name__, t / 1000. / 60.))
             else:
                 print('%r  %2.2f ms' %
                       (method.__name__, t))
 
         return result
+
     return timed
 
-def fix_path(path,dir='forward',verbose=False):
+
+def fix_path(path, dir='forward', verbose=False):
+    """
+    Corrects the path from UNIX filesystem \) -> ), etc...
+    :param path: Path to fix
+    :param dir: default is forward
+    :param verbose: print path
+    :return: corrected path
+    """
     path = repr(path)
     if dir == 'forward':
         path = path.replace(")", "\)")
@@ -79,9 +107,18 @@ def fix_path(path,dir='forward',verbose=False):
         pass
 
 
-# def find_div(x,lim):
-#     def divisible(m, n):
-#         return m % n == 0
-#     for i in range(lim): 
-#         if divisible(x,i+1): 
-#             print(i) 
+def find_div(x: int, lim: int) -> None:
+    """
+    Finds the divisible numbers of x
+    :param x: input number
+    :type x: integer
+    :param lim: find divisible up to lim
+    :return: None
+    """
+
+    def divisible(m, n):
+        return m % n == 0
+
+    for i in range(lim):
+        if divisible(x, i + 1):
+            print(i)
