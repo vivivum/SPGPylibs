eebf60541aca475ef62833a41b687a43a58b416f
Alex Feller <79581329+alexjfeller@users.noreply.github.com>
2022-11-10 14:09:12 +0100

    Fix pdoc issue

--- SPGPylibs/PHItools/cmilos/__init__.py
+++ SPGPylibs/PHItools/cmilos/__init__.py
@@ -3,3 +3,5 @@ try:
 except:
     print("unable to import pmilos in __init__.py in .PHItools.cmilos (this is o.k.)")
 
+# This is important for pdoc to ignore other files, like setup.py
+__all__ = ['pmilos']
